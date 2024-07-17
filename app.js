e531c6c768c12b1476d716ccdd73f2e49665c66f
Daniel-Oshione <omobsdaniel@gmail.com>
2024-07-17 21:03:23 +0100

    added menu box

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.getElementById('toggle');
+    const dropDown = document.querySelector('.menu');
+  
+    toggleButton.addEventListener('click', function() {
+      dropDown.classList.toggle('visible');
+    });
+  });
